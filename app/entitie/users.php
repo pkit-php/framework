e7339e2fe8bbf0c1ce56dfb175466fcab5481f83
kauaug <kauaug.mo@gmail.com>
2022-04-20 12:04:28 -0300

    feat: exemplor de uso da classe Table

---
+++ app/entitie/users.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entitie;
+
+use Pkit\Database\Table;
+
+class Users extends Table
+{
+  public
+    $id,
+    $name,
+    $email;
+  protected
+    $password;
+}
--- routes/users/(id)/index.php
+++ routes/users/(id)/index.php
@@ -1,12 +1,18 @@
 <?php
 
+use App\Entitie\Users;
 use Pkit\Http\Route;
 
 class UserById extends Route
 {
     public function get($request, $response)
     {
-        $response->json()->send($request->getRouter()->getParams());
+        $params = $request->getRouter()->getParams();
+        $user = (new Users);
+        $id = $params["id"];
+        $selecteds = $user->select(['id:=' => $id]);
+        echo $selecteds[0]->getProtectedValue('password') . "\n";
+        $response->json()->send($selecteds[0]);
     }
 }
 
