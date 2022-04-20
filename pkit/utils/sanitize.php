be237b4e8549d187ea6fba81f2ab00beb49f1665
kauaug <kauaug.mo@gmail.com>
2022-04-20 12:00:49 -0300

    refactor: sanitize em uma classe

---
+++ pkit/utils/sanitize.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pkit\Utils;
+
+class Sanitize
+{
+  static function sanitizeClass($class)
+  {
+    return @end(explode("\\", $class));
+  }
+
+  static function sanitizeURI(string $uri)
+  {
+    $uri_base = explode('?', $uri)[0];
+    $pure_uri = explode('#', $uri_base)[0];
+
+    $xUri = $pure_uri ?? '/';
+    $yUri = rtrim($xUri, '/');
+    if ($yUri == '') {
+      return '/';
+    }
+
+    return $yUri . '/';
+  }
+}
