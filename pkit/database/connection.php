24533527747888f78c4ce6ffbbd5f9e2559ebd53
kauaug <kauaug.mo@gmail.com>
2022-04-20 07:35:37 -0300

    refactor: sem responsabilidade de erro

--- pkit/database/connection.php
+++ pkit/database/connection.php
@@ -8,10 +8,9 @@ class Connection
 {
 
   private PDO $pdo;
-  private static $debug = false;
 
   private static string
-    $db,
+    $driver,
     $host,
     $dbname,
     $user,
@@ -19,44 +18,38 @@ class Connection
 
   public static function init(array $set)
   {
-    self::$db = $set[0];
-    self::$host = $set[1];
-    self::$dbname = $set[2];
-    self::$user = $set[3];
-    self::$pass = $set[4];
+    self::$driver = $set['driver'];
+    self::$host = $set['host'];
+    self::$dbname = $set['name'];
+    self::$user = $set['user'];
+    self::$pass = $set['pass'];
   }
 
 
   private function connect()
   {
-    $db = self::$db;
+    $driver = self::$driver;
     $host = self::$host;
     $dbname = self::$dbname;
     $user = self::$user;
     $pass = self::$pass;
-    $this->pdo = new PDO("$db:host=$host;dbname=$dbname", $user, $pass);
+    $this->pdo = new PDO("$driver:host=$host;dbname=$dbname", $user, $pass);
     // throw exceptions, when SQL error is caused
     $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // prevent emulation of prepared statements
     $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
   }
 
-  public function query($query, $values = [])
+  public function execute($query, $values = [])
   {
     $this->connect();
-    try {
-      $stmt = $this->pdo->prepare($query);
-      foreach ($values as $key => $value) {
-        $stmt->bindValue($key, $value);
-      }
-      $stmt->execute();
-    } catch (\Throwable $th) {
-      if (Self::$debug) {
-        echo $th;
-      } else {
-        throw $th;
-      }
+
+    $stmt = $this->pdo->prepare($query);
+    foreach ($values as $key => $value) {
+      $stmt->bindValue($key, $value);
     }
+    $stmt->execute();
+
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 }
