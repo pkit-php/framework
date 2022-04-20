9d2df37f428a71eea8be08ccce59e3a66a742eb7
kauaug <kauaug.mo@gmail.com>
2022-04-20 14:30:09 -0300

    fix: o .env nem sempre é necessário

--- pkit/utils/dotenv.php
+++ pkit/utils/dotenv.php
@@ -9,18 +9,11 @@ class DotEnv
 
   public function __construct(string $path)
   {
-    if (!file_exists($path)) {
-      throw new \InvalidArgumentException(sprintf('%s does not exist', $path));
-    }
     $this->path = $path;
   }
 
   public function load(): void
   {
-    if (!is_readable($this->path)) {
-      throw new \RuntimeException(sprintf('%s file is not readable', $this->path));
-    }
-
     $lines = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     foreach ($lines as $line) {
 
