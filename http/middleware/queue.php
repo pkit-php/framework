989ad118ee105fece7f6c51da10f021d1fde9c02
kauaug <kauaug.mo@gmail.com>
2022-04-22 09:31:28 -0300

    feat: middlewares mapeados por namespace

--- http/middleware/queue.php
+++ http/middleware/queue.php
@@ -1,18 +1,35 @@
-<?php namespace Pkit\Http\Middleware;
+<?php
+
+namespace Pkit\Http\Middleware;
 
 use Pkit\Http\Request;
 use Pkit\Http\Response;
 
 class Queue
 {
-  private static $map = [];
-  private
-    $middlewares,
-    $controller;
+  private static $namespace = '\App\Middlewares';
+  private array $middlewares;
+  private \Closure $controller;
 
-  public static function setMap(array $map)
+  public static function init(string $namespace)
   {
-    self::$map = $map;
+    self::$namespace = $namespace;
+  }
+
+  private static function getNamespace($class)
+  {
+    $path = explode("/", $class);
+    $path = array_map(function ($value) {
+      return ucfirst($value);
+    }, $path);
+    if (substr($class, 0, 4) === "pkit") {
+      unset($path[0]);
+      $baseClass = implode('\\', $path);
+      return 'Pkit\\Http\\Middlewares\\' .  $baseClass;
+    } else {
+      $baseClass = implode('\\', $path);
+      return self::$namespace . '\\' . $baseClass;
+    }
   }
 
   public function __construct(
@@ -31,15 +48,12 @@ class Queue
 
     $middleware = array_shift($this->middlewares);
 
-    if (!isset(self::$map[$middleware])) {
-      return $response->error(500)->send();
-    }
-
     $queue = $this;
     $next = function ($request, $response) use ($queue) {
       return $queue->next($request, $response);
     };
 
-    return (new self::$map[$middleware])->handle($request, $response, $next);
+    $namespace = self::getNamespace($middleware);
+    return (new $namespace())->handle($request, $response, $next);
   }
 }
