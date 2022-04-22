cbe71f9ecca50c7c648b677dcf30411026fa1b0c
kauaug <kauaug.mo@gmail.com>
2022-04-22 12:52:31 -0300

    feat: middleware de sessão

---
+++ http/middlewares/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pkit\Http\Middlewares;
+
+use Pkit\Abstracts\Middleware;
+use Pkit\Utils\Session;
+
+class Auth implements Middleware
+{
+  public function handle($request, $response, $next)
+  {
+    if (Session::logged()) {
+      return $next($request, $response);
+    };
+    $response->unauthorized()->send();
+  }
+}
