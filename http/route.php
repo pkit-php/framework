69c308e1b0dd027ffadeb6292e8c0fb719c2b0c4
kauaug <kauaug.mo@gmail.com>
2022-04-22 17:03:49 -0300

    refactor: função movida  e extends retirado

--- http/route.php
+++ http/route.php
@@ -1,69 +1,33 @@
 <?php namespace Pkit\Http;
 
-use Pkit\Abstracts\Route as AbstractsRoute;
-use Pkit\Http\Middleware\Queue;
+use Pkit\Http\Middlewares;
 use Pkit\Http\Request;
 use Pkit\Http\Response;
 use Pkit\Http\Router;
 use Pkit\Utils\Methods;
 
-class Route extends AbstractsRoute
+class Route
 {
-  private function getMiddlewares($middlewares, $method)
-  {
-    $newMiddlewares = [];
-    foreach ($middlewares as $key => $middleware) {
-      if (is_int($key)) {
-        $newMiddlewares[] = $middleware;
-      }
-    }
-    $methodsMiddlewares = $middlewares[strtolower($method)] ?? [];
-    return array_merge($newMiddlewares, $methodsMiddlewares);
-  }
+  public $middlewares;
 
   public function run()
   {
     $request = new Request(Router::$router);
     $response = new Response;
+    $middlewares = Middlewares::getMiddlewares($this->middlewares ?? [], $request->getHttpMethod());
 
-    $middlewares = $this->getMiddlewares($this->middlewares ?? [], $request->getHttpMethod());
-
-    (new Queue(function ($request, $response) {
+    (new Middlewares(function ($request, $response) {
       $this->runMethod($request, $response);
     }, $middlewares))->next($request, $response);
   }
 
   private function runMethod($request, $response)
   {
-
-    switch ($request->getHttpMethod()) {
-      case 'GET':
-        $this->get($request, $response);
-        break;
-      case 'HEAD':
-        $this->head($request, $response);
-        break;
-      case 'PUT':
-        $this->put($request, $response);
-        break;
-      case 'POST':
-        $this->post($request, $response);
-        break;
-      case 'PATCH':
-        $this->patch($request, $response);
-        break;
-      case 'TRACE':
-        $this->trace($request, $response);
-        break;
-      case 'OPTIONS':
-        $this->options($request, $response);
-        break;
-      case 'DELETE':
-        $this->delete($request, $response);
-        break;
-      default:
-        Methods::methodNotAllowed($request, $response);
-        break;
+    $method = strtolower($request->getHttpMethod());
+    if(method_exists($this, $method)){
+      $this->$method($request, $response);
+    } else {
+      Methods::methodNotAllowed($request, $response);
     }
   }
 }
