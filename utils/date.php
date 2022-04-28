0cafa3b9c5b8410a13fcf09735e40e896520cfba
kauaug <kauaug.mo@gmail.com>
2022-04-27 21:27:47 -0300

    refactor: calculo trabalhoso

--- utils/date.php
+++ utils/date.php
@@ -2,23 +2,12 @@
 
 namespace Pkit\Utils;
 
-use DateInterval;
 use DateTime;
 
 class Date
 {
-    static public function dateIntervalToSeconds(DateInterval $delta)
-    {
-        return ($delta->s)
-            + ($delta->i * 60)
-            + ($delta->h * 60 * 60)
-            + ($delta->d * 60 * 60 * 24)
-            + ($delta->m * 60 * 60 * 24 * 30)
-            + ($delta->y * 60 * 60 * 24 * 365);
-    }
-
     static public function deltaTime(DateTime $after, DateTime $before)
     {
-        return $after->diff($before);
+        return - ($after->getTimestamp() - $before->getTimestamp());
     }
 }
