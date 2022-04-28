2a8fd9034fcfc1fde73ecc32d63b9a31b23b8ed3
kauaug <kauaug.mo@gmail.com>
2022-04-27 21:15:20 -0300

    feat: jwt nos cabeçalhos e expiração

--- auth/jwt.php
+++ auth/jwt.php
@@ -2,29 +2,38 @@
 
 namespace Pkit\Auth;
 
+use DateTime;
+use Pkit\Http\Request;
+use Pkit\Http\Response;
 use Pkit\Utils\Base64url;
+use Pkit\Utils\Text;
 
 class Jwt
 {
-  private static $key;
+  private static $key, $expire = 0;
 
-  public static function init($key)
+  public static function init(string $key, $expire = 0)
   {
     self::$key = $key;
+    self::$expire = $expire;
   }
 
-  private static function signature($header, $payload)
+  private static function signature(string $header, string $payload)
   {
     $signature = hash_hmac('sha256', "$header.$payload", self::$key, true);
     return Base64url::encode($signature);
   }
 
-  public static function tokenize($payload)
+  public static function tokenize(array $payload)
   {
     $header = [
       'alg' => 'HS256',
       'typ' => 'JWT'
     ];
+
+    if (self::$expire) {
+      $payload['_created'] = (new DateTime())->format('Y-m-d H:i:s');
+    }
 
     $header = json_encode($header);
     $header = Base64url::encode($header);
@@ -37,14 +46,30 @@ class Jwt
     return "$header.$payload.$signature";
   }
 
-  public static function getPayload($token)
+  public static function getPayload(string $token)
   {
     $part = explode(".", $token);
     $payload = Base64url::decode($part[1]);
     return json_decode($payload);
   }
 
-  public static function validate($token)
+  public static function setBearer(Response $response, string $token)
+  {
+    $response->addHeader("authorization", "Bearer " . $token);
+  }
+
+  public static function getBearer(Request $request)
+  {
+    $auhorization = $request->getHeader("authorization") ?? "";
+    return Text::removeFromStart($auhorization, "Bearer ");
+  }
+
+  public static function getExpire()
+  {
+    return self::$expire;
+  }
+
+  public static function validate(string $token)
   {
     $part = explode(".", $token);
     $header = $part[0];
