e528dd03c72da3fccc924683a20258a0c8f28f4c
kauaug <kauaug.mo@gmail.com>
2022-06-27 08:40:21 -0300

    perf(private/routes): regex otimizado

--- src/Private/Routes.php
+++ src/Private/Routes.php
@@ -5,9 +5,9 @@ namespace Pkit\Private;
 class Routes
 {
     private static
-        $patternGeral = '/\[(.*?)\]|\[\.\.\.(.*?)\]/',
-        $patternVariable = '/\[(.*?)\]/',
-        $patternRest = '/\[\\\\.\\\\.\\\\.(.*?)\]/';
+        $patternVariable = '/\[\w+\]/',
+        $patternRest = '/\[\.{3}\w+\]/',
+        $patternGeral = '/\[(?:\.{3})?(\w+)\]/';
 
     public static function mathRoutes(array $routes, string $uri)
     {
@@ -26,33 +26,31 @@ class Routes
 
     public static function mathRouteAndParams(string $route, string $uri)
     {
-        $variables = [];
-        $route = str_replace('\\', '\\\\', $route);
-        $route = str_replace('*', '\*', $route);
-        $route = str_replace('(', '\(', $route);
-        $route = str_replace(')', '\)', $route);
-        $route = str_replace('{', '\{', $route);
-        $route = str_replace('}', '\}', $route);
-        $route = str_replace('.', '\.', $route);
-        $route = '/^' . str_replace('/', '\/', $route) . '$/';
         if (preg_match_all(self::$patternGeral, $route, $matches)) {
-            $route = preg_replace(self::$patternRest, '(.*)', $route);
-            $route = preg_replace(self::$patternVariable, '([^\/]*)', $route);
-            $variables = $matches[0];
-            $variables = array_map(function ($var) {
-                $var = trim($var, '\[.]');
-                return $var;
-            }, $variables);
+            $variables = $matches[1];
+            $regex = [
+                self::$patternRest => "\1",
+                self::$patternVariable => "\2",
+            ];
+            $route = preg_replace(
+                array_keys($regex),
+                array_values($regex),
+                $route
+            );
         }
 
-        if (preg_match($route, $uri)) {
+        $route = preg_quote($route);
+        $route = str_replace('/', '\/', $route);
+        $route = strtr($route, [
+            "\1" => '(.*)',
+            "\2" => '([^\/]*)',
+        ]);
+        $route = '/^' . $route . '$/';
 
-            if (preg_match($route, $uri, $matches)) {
-                unset($matches[0]);
-                $params = array_combine($variables, $matches);
-            };
-
-            return $params ?? [];
+        if (preg_match($route, $uri, $matches)) {
+            unset($matches[0]);
+            $params = array_combine($variables ?? [], $matches);
+            return $params;
         };
 
         return false;
