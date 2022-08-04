037fd3c6f8f6c5f21d838be2b0dbcd15b86da971
kauaug <kauaug.mo@gmail.com>
2022-08-03 23:28:10 -0300

    fix(utils-Text): texto transbordando

--- src/Utils/Text.php
+++ src/Utils/Text.php
@@ -4,18 +4,24 @@ namespace Pkit\Utils;
 
 class Text
 {
-  static function removeFromEnd(string $haystack, string $needle)
+  static function removeFromEnd(string $haystack, string $needle): string
   {
     $length = strlen($needle);
+    if($length > strlen($haystack)){
+      return $haystack;
+    }
     if (substr($haystack, -$length) === $needle) {
       $haystack = substr($haystack, 0, -$length);
     }
     return $haystack;
   }
 
-  static function removeFromStart(string $haystack, string $needle)
+  static function removeFromStart(string $haystack, string $needle): string
   {
     $length = strlen($needle);
+    if($length > strlen($haystack)){
+      return $haystack;
+    }
     if (substr($haystack, 0, $length) === $needle) {
       $haystack = substr($haystack, $length);
     }
