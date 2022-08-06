a3c4c5dd453557a0666c1b54b490eb2dfdbf2899
kauaug <kauaug.mo@gmail.com>
2022-08-06 10:31:28 -0300

    feat(Http-ContentType): método de validação

--- src/Http/ContentType.php
+++ src/Http/ContentType.php
@@ -4,6 +4,12 @@ namespace Pkit\Http;
 
 class ContentType
 {
+  public static function validate(string $content)
+  {
+    $constantNames = (new \ReflectionClass(self::class))
+      ->getConstants();
+    return in_array($content, $constantNames);
+  }
   const NONE = "*";
   const JSON = "application/json";
   const HTML = "text/html";
