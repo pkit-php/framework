40f649a15007a8e60ac08b9b25038920e3fbb128
kauaug <kauaug.mo@gmail.com>
2022-08-06 10:33:23 -0300

    feat(Throwable): Erro

---
+++ src/Throwable/Error.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pkit\Throwable;
+
+use Pkit\Http\Status;
+
+class Error extends \Exception
+{
+    public function __construct(string $message, int $code)
+    {
+        if (
+            $code < 400 ||
+            $code >= 600 ||
+            !Status::validate($code)
+        ) {
+            throw new Error("Error: Status '$code' is not valid", 500);
+        }
+        parent::__construct($message, $code);
+    }
+}
