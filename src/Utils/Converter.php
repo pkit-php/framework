d57edebd7d7817c66f02d053925da71cd62bac58
kauaug <kauaug.mo@gmail.com>
2022-08-08 21:42:34 -0300

    feat(Converter): xml

--- src/Utils/Converter.php
+++ src/Utils/Converter.php
@@ -2,6 +2,8 @@
 
 namespace Pkit\Utils;
 
+use Pkit\Throwable\Error;
+
 class Converter
 {
 
@@ -21,9 +23,42 @@ class Converter
     return $array;
   }
 
-  public static function xmlToArray(string $xml)
+  public static function xmlToArray(string $xml): array
   {
-    $string = simplexml_load_string($xml);
-    return json_decode(json_encode($string), 1);
+    $_xml = new \SimpleXMLElement($xml);
+      
+    $array = json_decode(json_encode($_xml), 1);
+    $root = $_xml->getName();
+    if($root !== "root"){
+      $array = ["@root" => $root,...$array];
+    }
+    return $array;
+  }
+
+  public static function arrayToXml(array $array): string
+  {
+    
+    $rootElement = @$array["@root"] ?? "root";
+    unset($array["@root"]);
+
+    $xml = new \SimpleXMLElement("<$rootElement/>");
+    self::handleArrayToXml($array, $xml);
+
+    return $xml->asXML();
+  }
+
+  private static function handleArrayToXml(array $array, \SimpleXMLElement $xml)
+  {
+    foreach ($array as $tag => $children) {
+      if ($tag == "@attributes") {
+        foreach ($children as $attribute => $valueAttribute) {
+          $xml->addAttribute($attribute, $valueAttribute);
+        }
+      } else if (is_array($children)) {
+        self::handleArrayToXml($children, $xml->addChild($tag));
+      } else {
+        $xml->addChild($tag, $children);
+      }
+    }
   }
 }
