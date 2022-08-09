f83ab4320920eb823f299cdd9f2c791ec0a12635
kauaug <kauaug.mo@gmail.com>
2022-08-08 21:34:48 -0300

    refactor(Database-QueryBuilder): limit compativel

--- src/Database/QueryBuilder.php
+++ src/Database/QueryBuilder.php
@@ -325,7 +325,7 @@ class QueryBuilder
   {
     $offset = array_keys($command["limit"])[0];
     $row_count = array_values($command["limit"])[0];
-    $this->query .= "LIMIT $offset, $row_count ";
+    $this->query .= "OFFSET $offset ROWS FETCH NEXT $row_count ROWS ONLY";
   }
 
   public function return(array $return)
