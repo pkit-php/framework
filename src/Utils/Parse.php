b1ef42d66f39460ae5143027e87d56d4d95c2732
kauaug <kauaug.mo@gmail.com>
2022-08-11 17:23:39 -0300

    feat(Utils-Parse): header to array

--- src/Utils/Parse.php
+++ src/Utils/Parse.php
@@ -15,6 +15,27 @@ class Parse
     return $any;
   }
 
+  public static function headerToArray(string $header, $recursive = true): array
+  {
+    $return = [];
+    foreach (explode(";", $header) as $line) {
+      $return[] = array_map(function ($parse) {
+        parse_str($parse, $result);
+        if (
+          count($result) == 1 &&
+          in_array("", $result)
+        )
+          return array_keys($result)[0];
+        return $result;
+      }, explode(",", $line));
+    }
+
+    if ($recursive)
+      return $return;
+
+    return array_merge(...$return);
+  }
+
   public static function objectToArray(object $object)
   {
     foreach ($object as $key => $value) {
@@ -26,18 +47,18 @@ class Parse
   public static function xmlToArray(string $xml): array
   {
     $_xml = new \SimpleXMLElement($xml);
-      
+
     $array = json_decode(json_encode($_xml), 1);
     $root = $_xml->getName();
-    if($root !== "root"){
-      $array = ["@root" => $root,...$array];
+    if ($root !== "root") {
+      $array = ["@root" => $root, ...$array];
     }
     return $array;
   }
 
   public static function arrayToXml(array $array): string
   {
-    
+
     $rootElement = @$array["@root"] ?? "root";
     unset($array["@root"]);
 
