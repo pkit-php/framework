a8794c49e32116ecb01c8419037ce90fd2f5ee56
kauan <kauan.oliveira@dantas.eletro.com>
2022-08-12 12:42:35 -0300

    feat(Utils-Sanitize): mais métodos de sanitização

--- src/Utils/Sanitize.php
+++ src/Utils/Sanitize.php
@@ -33,4 +33,44 @@ class Sanitize
     }
     return $array;
   }
+
+  public static function url(string $url): string
+  {
+    return filter_var($url, FILTER_SANITIZE_URL);
+  }
+
+  public static function int(string $int): string
+  {
+    return filter_var($int, FILTER_SANITIZE_NUMBER_INT);
+  }
+
+  public static function email(string $email): string
+  {
+    return filter_var($email, FILTER_SANITIZE_EMAIL);
+  }
+
+  public static function float(string $float): string
+  {
+    return filter_var($float, FILTER_SANITIZE_NUMBER_FLOAT);
+  }
+
+  public static function encoded(string $encoded): string
+  {
+    return filter_var($encoded, FILTER_SANITIZE_ENCODED);
+  }
+
+  public static function slashes(string $slashes): string
+  {
+    return filter_var($slashes, FILTER_SANITIZE_ADD_SLASHES);
+  }
+
+  public static function special_chars(string $special_chars): string
+  {
+    return filter_var($special_chars, FILTER_SANITIZE_SPECIAL_CHARS);
+  }
+
+  public static function full_especial_chars(string $full_especial_chars): string
+  {
+    return filter_var($full_especial_chars, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  }
 }
